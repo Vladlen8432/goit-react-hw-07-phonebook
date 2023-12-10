0898336bfe3a44d556c2b984ccb3ed0e1b3bae8a
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2023-12-10 19:57:22 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,41 @@
+// import { useEffect } from 'react';
+// import { PersistGate } from 'redux-persist/integration/react';
+// import { persistor } from './Redux/Store';
+// import ContactForm from './ContactForm/ContactForm';
+// import ContactList from './ContactList/ContactList';
+// import SearchInput from './FilterByName/FilterByName';
+
 import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor } from './Redux/Store';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import SearchInput from './FilterByName/FilterByName';
+import { fetchContacts } from './Redux/ContactSlice';
 
 const App = () => {
+  const dispatch = useDispatch();
+  const { isLoading, error } = useSelector(state => state);
+  // const isLoading = useSelector(state => state.contacts.isLoading);
+  // const error = useSelector(state => state.contacts.error);
+
   useEffect(() => {
+    dispatch(fetchContacts());
     persistor.persist();
-  }, []);
+  }, [dispatch]);
+
+  if (isLoading === undefined) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  // useEffect(() => {
+  //   persistor.persist();
+  // }, []);
 
   return (
     <PersistGate loading={null} persistor={persistor}>
--- src/components/Redux/ContactSlice.jsx
+++ src/components/Redux/ContactSlice.jsx
@@ -1,10 +1,24 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchContacts = createAsyncThunk('contacts/fetchAll', async () => {
+  try {
+    const response = await fetch(
+      'https://656657da64fcff8d730eb5c2.mockapi.io/api/vshark/contacts'
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error('Failed to fetch contacts');
+  }
+});
 
 const ContactSlice = createSlice({
   name: 'contacts',
   initialState: {
     items: [],
-    blacklist: ['filter'],
+    isLoading: false,
+    error: null,
+    // blacklist: ['filter'],
     filter: '',
   },
   reducers: {
@@ -19,6 +33,21 @@ const ContactSlice = createSlice({
     updateFilter: (state, action) => {
       state.filter = action.payload;
     },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchContacts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
