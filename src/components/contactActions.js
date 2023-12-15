359f7bf8b76ee25cdf25098817f4b3cb37b05bc0
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2023-12-15 23:03:45 +0200

    test

--- src/components/contactActions.js
+++ src/components/contactActions.js
@@ -1,7 +1,8 @@
 import { nanoid } from 'nanoid';
-import { addContact, deleteContact, updateFilter } from './Redux/ContactSlice';
+import { addContact, deleteContact } from './Redux/ContactSlice';
+// import { addContact, deleteContact, updateFilter } from './Redux/ContactSlice';
 
-export const handleAddContact = (
+export const handleAddContact = async (
   dispatch,
   contacts,
   name,
@@ -9,9 +10,7 @@ export const handleAddContact = (
   setName,
   setNumber
 ) => {
-  if (
-    contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())
-  ) {
+  if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
     alert(`${name} is already in contacts`);
   } else {
     const newContact = {
@@ -19,17 +18,75 @@ export const handleAddContact = (
       name: name,
       number: number,
     };
-    dispatch(addContact(newContact));
 
-    setName('');
-    setNumber('');
+    try {
+      const response = await fetch('https://656657da64fcff8d730eb5c2.mockapi.io/api/vshark/contacts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newContact),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to add contact to the backend');
+      }
+
+      dispatch(addContact(newContact));
+
+      setName('');
+      setNumber('');
+    } catch (error) {
+      console.error(error);
+    }
   }
 };
 
-export const handleDeleteContact = (dispatch, id) => {
-  dispatch(deleteContact(id));
+export const handleDeleteContact = async (dispatch, id) => {
+  try {
+    const response = await fetch(`https://656657da64fcff8d730eb5c2.mockapi.io/api/vshark/contacts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to delete contact from the backend');
+    }
+
+    dispatch(deleteContact(id));
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const handleFilterChange = (dispatch, value) => {
-  dispatch(updateFilter(value));
-};
+// export const handleAddContact = (
+//   dispatch,
+//   contacts,
+//   name,
+//   number,
+//   setName,
+//   setNumber
+// ) => {
+//   if (
+//     contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())
+//   ) {
+//     alert(`${name} is already in contacts`);
+//   } else {
+//     const newContact = {
+//       id: nanoid(),
+//       name: name,
+//       number: number,
+//     };
+//     dispatch(addContact(newContact));
+
+//     setName('');
+//     setNumber('');
+//   }
+// };
+
+// export const handleDeleteContact = (dispatch, id) => {
+//   dispatch(deleteContact(id));
+// };
+
+// export const handleFilterChange = (dispatch, value) => {
+//   dispatch(updateFilter(value));
+// };
