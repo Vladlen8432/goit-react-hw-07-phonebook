79941071339245d7c51b02d37960a2a79a426e24
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2023-12-17 22:58:48 +0200

    update-finish

--- src/components/contactActions.js
+++ src/components/contactActions.js
@@ -10,7 +10,9 @@ export const handleAddContact = async (
   setName,
   setNumber
 ) => {
-  if (contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())) {
+  if (
+    contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())
+  ) {
     alert(`${name} is already in contacts`);
   } else {
     const newContact = {
@@ -20,13 +22,16 @@ export const handleAddContact = async (
     };
 
     try {
-      const response = await fetch('https://657cc619853beeefdb99f3c6.mockapi.io/vlasark/contacts', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newContact),
-      });
+      const response = await fetch(
+        'https://657cc619853beeefdb99f3c6.mockapi.io/vlasark/contacts',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newContact),
+        }
+      );
 
       if (!response.ok) {
         throw new Error('Failed to add contact to the backend');
@@ -44,9 +49,12 @@ export const handleAddContact = async (
 
 export const handleDeleteContact = async (dispatch, id) => {
   try {
-    const response = await fetch(`https://657cc619853beeefdb99f3c6.mockapi.io/vlasark/contacts/${id}`, {
-      method: 'DELETE',
-    });
+    const response = await fetch(
+      `https://657cc619853beeefdb99f3c6.mockapi.io/vlasark/contacts/${id}`,
+      {
+        method: 'DELETE',
+      }
+    );
 
     if (!response.ok) {
       throw new Error('Failed to delete contact from the backend');
@@ -57,36 +65,3 @@ export const handleDeleteContact = async (dispatch, id) => {
     console.error(error);
   }
 };
-
-// export const handleAddContact = (
-//   dispatch,
-//   contacts,
-//   name,
-//   number,
-//   setName,
-//   setNumber
-// ) => {
-//   if (
-//     contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())
-//   ) {
-//     alert(`${name} is already in contacts`);
-//   } else {
-//     const newContact = {
-//       id: nanoid(),
-//       name: name,
-//       number: number,
-//     };
-//     dispatch(addContact(newContact));
-
-//     setName('');
-//     setNumber('');
-//   }
-// };
-
-// export const handleDeleteContact = (dispatch, id) => {
-//   dispatch(deleteContact(id));
-// };
-
-// export const handleFilterChange = (dispatch, value) => {
-//   dispatch(updateFilter(value));
-// };
